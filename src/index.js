243c744b1713a07c8a2ad68ee39589cb599e820a
Iswan Jumat <iswan@thinkcube.com>
2016-11-13 20:22:34 +0530

    removed all create-react-app default files and added hot reload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './containers/App';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+//react hot reloader hack for components until create-react-app supports
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    const NextRoot = require('./containers/App').default;
+    ReactDOM.render(
+      <NextRoot />,
+      document.getElementById('root')
+    );
+  });
+}
